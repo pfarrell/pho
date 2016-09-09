d4af45887ccf36cf69b0a7bbda64a2f07dbd29fb
Patrick Farrell <mr.patfarrell@gmail.com>
2016-09-09 00:41:24 -0700

    first page partials

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,12 @@ class Photo < Sequel::Model
   many_to_one :camera
   one_to_one :file
   many_to_many :tags
+  
+  def file_name
+    Pathname.new(self.path).basename
+  end
+  
+  def thumbnail
+    "public/thumbnails/#{file_name}-thumb.jpg"
+  end
 end

9120397af766e412bce7d2a7c96abf71575a3192
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-09-20 18:57:05 -0700

    rename require to new name rmagick

--- app/models.rb
+++ app/models.rb
@@ -19,7 +19,7 @@ DB.pool.connection_validation_timeout = 300
 Sequel::Model.plugin :timestamps
 Sequel::Model.plugin :json_serializer
 
-require 'RMagick'
+require 'rmagick'
 require 'models/camera'
 require 'models/file'
 require 'models/photo'

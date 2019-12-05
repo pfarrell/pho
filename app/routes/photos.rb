208918937b8cac19faeabd0b772d588e135fba1d
Patrick Farrell <pat@cursor.com>
2019-12-05 00:19:22 -0800

    reverse the photos that precede the current photo in pagination

--- app/routes/photos.rb
+++ app/routes/photos.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Application
   get "/photo/:id" do
     protected
     curr = Photo[params[:id].to_i]
-    prev = Photo.where(Sequel.lit('date < ?', curr.date)).exclude(id: curr.id).order(:date).last(3)
+    prev = Photo.where(Sequel.lit('date < ?', curr.date)).exclude(id: curr.id).order(:date).last(3).reverse
     nxt = Photo.where(Sequel.lit('date > ?', curr.date)).exclude(id: curr.id).order(:date).first(3)
     haml :photo, locals: {photo: curr, nxt: nxt, prev: prev, user_id: @user.id}.merge(symbolize_keys(params))
   end

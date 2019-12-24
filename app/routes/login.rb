005f90c78f64e8e1c006f9e990432c726da57d65
Patrick Farrell <pat@cursor.com>
2019-12-24 09:37:44 -0800

    error handling on bad logins

--- app/routes/login.rb
+++ app/routes/login.rb
@@ -1,21 +1,25 @@
 class App < Sinatra::Application
 
   post "/login" do
-    user = User.find(username: params[:username])
-    raise Exception('user not found') if user.nil?
     begin
-      user.check_password(params[:password])
+      user = User.find(username: params[:username])
+      raise Exception('user not found') if user.nil?
+      begin
+        user.check_password(params[:password])
+      rescue
+        haml :login, locals: {error_message: 'Try again'}
+      end
+      token = generate_token(user)
+      respond_to do |f|
+        f.json { {:token=>token}.to_json }
+        f.html {
+          cookies[:auth] = token
+          destination = params[:return_to] || "/rebecca"
+          redirect url_for(destination)
+        }
+      end
     rescue
-        haml :login
-    end
-    token = generate_token(user)
-    respond_to do |f|
-      f.json { {:token=>token}.to_json }
-      f.html {
-        cookies[:auth] = token
-        destination = params[:return_to] || "/rebecca"
-        redirect url_for(destination)
-      }
+      haml :login, locals: {error_message: 'Try again'}
     end
   end
 end
